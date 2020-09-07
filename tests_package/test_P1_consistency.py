8e0c074e4c32ef5206b5cd646b40807e93e775ea
Frederic Marazzato <frederic.marazzato@enpc.fr>
2020-09-07 11:10:59 -0500

    Tests for 2d and 3d reconstructions look okay. Continue writing tests and exceptions and also to make a more functionnal code.

--- tests_package/test_P1_consistency.py
+++ tests_package/test_P1_consistency.py
@@ -2,36 +2,38 @@
 import sys
 sys.path.append('../')
 from DEM import *
+import pytest
 
 #import pytest #for unit tests
-eps = 1e-15 #constant to compare floats. Possible to find a Python constant with that value ?
+eps = 2e-15 #constant to compare floats. Possible to find a Python constant with that value ?
 eps_2 = 1e-12 #constant to compare gradients to zero
 
 #Size of mesh and number of elements
 L = 0.5
 nb_elt = 3
 
-def test_reconstruction(mesh_):
-    dim = mesh_.geometric_dimension()
+@pytest.mark.parametrize("mesh", [RectangleMesh(Point(-L,-L),Point(L,L),nb_elt,nb_elt,"crossed"), BoxMesh(Point(-L, -L, -L), Point(L, L, L), nb_elt, nb_elt, nb_elt)])
+def test_reconstruction(mesh):
+    dim = mesh.geometric_dimension()
     d = dim #scalar problem #dim #vectorial problem
 
     #DEM reconstruction
-    DEM_to_DG, DEM_to_CG, DEM_to_CR, DEM_to_DG_1, nb_dof_DEM = compute_all_reconstruction_matrices(mesh_, d)
+    DEM_to_DG, DEM_to_CG, DEM_to_CR, DEM_to_DG_1, nb_dof_DEM = compute_all_reconstruction_matrices(mesh, d)
     #print('nb dof DEM: %i' % nb_dof_DEM)
 
     #Testing P1 consistency and that's all
-    x = SpatialCoordinate(mesh_)
-    u = DEM_interpolation(x, mesh_, d, DEM_to_CG, DEM_to_DG)
+    x = SpatialCoordinate(mesh)
+    u = DEM_interpolation(x, mesh, d, DEM_to_CG, DEM_to_DG)
     assert abs(max(u) - L) < eps
     assert abs(min(u) + L) < eps
     #Ajouter des test unitaires là-dessus.
 
     #Functional Spaces
-    U_DG = VectorFunctionSpace(mesh_, 'DG', 0) #Pour délacement dans cellules
-    U_DG_1 = VectorFunctionSpace(mesh_, 'DG', 1)
-    U_CR = VectorFunctionSpace(mesh_, 'CR', 1) #Pour interpollation dans les faces
-    U_CG = VectorFunctionSpace(mesh_, 'CG', 1) #Pour bc
-    W = TensorFunctionSpace(mesh_, 'DG', 0)
+    U_DG = VectorFunctionSpace(mesh, 'DG', 0) #Pour délacement dans cellules
+    U_DG_1 = VectorFunctionSpace(mesh, 'DG', 1)
+    U_CR = VectorFunctionSpace(mesh, 'CR', 1) #Pour interpollation dans les faces
+    U_CG = VectorFunctionSpace(mesh, 'CG', 1) #Pour bc
+    W = TensorFunctionSpace(mesh, 'DG', 0)
 
     #CR interpolation
     test_CR = Function(U_CR)
@@ -48,6 +50,7 @@ def test_reconstruction(mesh_):
     assert abs(min(gradient_vec[:,0,1])) < eps_2 and abs(max(gradient_vec[:,0,1])) < eps_2
     assert abs(min(gradient_vec[:,1,0])) < eps_2 and abs(max(gradient_vec[:,1,0])) < eps_2
     assert abs(min(gradient_vec[:,1,1]) - 1.) < eps_2 and abs(max(gradient_vec[:,1,1]) - 1.) < eps_2
+    #Ajouter cas pour 3d
 
     #Outputfile
     #file = File('P1_consistency.pvd')
@@ -68,20 +71,23 @@ def test_reconstruction(mesh_):
     assert abs(min(gradient_vec[:,0,1])) < eps_2 and abs(max(gradient_vec[:,0,1])) < eps_2
     assert abs(min(gradient_vec[:,1,0])) < eps_2 and abs(max(gradient_vec[:,1,0])) < eps_2
     assert abs(min(gradient_vec[:,1,1]) - 1.) < eps_2 and abs(max(gradient_vec[:,1,1]) - 1.) < eps_2
+    #Ajouter cas pour 3d
 
 
     #Outputfile
     #file.write(test_DG_1)
     #file.write(gradient_DG)
 
-    return 0
 
-
-#Testing reconstructions in both 2d and 3d
-def test_2d():
-    mesh = RectangleMesh(Point(-L,-L),Point(L,L),nb_elt,nb_elt,"crossed")
-    assert test_reconstruction(mesh) == 0
-
-def test_3d():
-    mesh = BoxMesh(Point(0., 0., 0.), Point(L, L, L), nb_elt, nb_elt, nb_elt)
-    assert test_reconstruction(mesh) == 0
+##Testing reconstructions in both 2d and 3d
+#@pytest.fixture
+#def mesh_for_test(): #2d mesh
+#    #mesh = RectangleMesh(Point(-L,-L),Point(L,L),nb_elt,nb_elt,"crossed")
+#    #assert test_reconstruction(mesh) == 0
+#    return RectangleMesh(Point(-L,-L),Point(L,L),nb_elt,nb_elt,"crossed")
+#
+#@pytest.fixture
+#def mesh_for_test(): #3d mesh
+#    #mesh = BoxMesh(Point(-L, -L, -L), Point(L, L, L), nb_elt, nb_elt, nb_elt)
+#    #assert test_reconstruction(mesh) == 0
+#    return BoxMesh(Point(-L, -L, -L), Point(L, L, L), nb_elt, nb_elt, nb_elt)
